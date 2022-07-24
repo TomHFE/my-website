c4a0bc83afe5366b4bd51b23f0957eb55cababf3
davechapelle <98556082+davechapelle@users.noreply.github.com>
2022-07-24 18:37:00 +0100

    Update PortfolioPage.js

--- src/portfolioPage/PortfolioPage.js
+++ src/portfolioPage/PortfolioPage.js
@@ -54,24 +54,20 @@ const PortfolioPage = () => {
           className="mov"
           onHoverStart={{ opacity: 1 }}
         >
-          <h1>Movie App</h1>
+          <h1>Website</h1>
           <MovLinks />
           <div className="mov-text">
             <p>
-              React based application for Films and TV shows. This App enables
-              the user to search for movies by their name. This then fetches to
-              an api which looks for information related to the film such as
-              plot, runtime and actors/directors involved with the film.
+              Portfolio site with responsive user interface built with framer
+              motion, react router v6 used for linking pages along with anchor
+              tags to Linkedin, Github, EmailJS compatability and a downloadable
+              CV.
             </p>
             <h3>Features</h3>
             <ul>
-              <li>Input search to an API through an async function</li>
-              <li>Error handling and redirect</li>
+              <li>Responsive web design</li>
+              <li>EmailJS</li>
               <li>Animated transitions and flex compatibility</li>
-              <li>
-                Props sent through UseParams linked to another API allowing for
-                the user to search details about the movie
-              </li>
               <li>
                 React Router capabilities including linking, redirect, UseParams
                 and routes
